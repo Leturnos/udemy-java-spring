3b3f06133683b0e103ca8973d65168bd185a5564
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-02-14 10:06:38 -0300

    sec-19-initial setup for solution of challenge 1: Create tests for XML

---
+++ 16-patch-verb/solution-challenge-1-xml/src/main/java/com/udemy/spring_boot/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.udemy.spring_boot.repository;
+
+import com.udemy.spring_boot.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {}
