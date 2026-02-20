c34fab99de2ed63f21a2e99286dee400187f2730
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-02-20 17:13:54 -0300

    sec-19-solution to challenge 2

---
+++ 16-patch-verb/solution-challenge-2-yaml/src/test/java/com/udemy/spring_boot/integrationtests/controllers/withyaml/PersonControllerYamlTest.java
@@ -0,0 +1,236 @@
+package com.udemy.spring_boot.integrationtests.controllers.withyaml;
+
+import com.udemy.spring_boot.config.TestConfigs;
+import com.udemy.spring_boot.integrationtests.controllers.withyaml.mapper.YAMLMapper;
+import com.udemy.spring_boot.integrationtests.dto.PersonDTO;
+import com.udemy.spring_boot.integrationtests.testcontainer.AbstractIntegrationTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PersonControllerYamlTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static YAMLMapper objectMapper;
+
+    private static PersonDTO person;
+
+    @BeforeAll
+    static void setUp() {
+        objectMapper = new YAMLMapper();
+        person = new PersonDTO();
+    }
+
+    @Test
+    @Order(1)
+    void createTest() {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+            .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_GOOGLE)
+            .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+            .build();
+
+        var createdPerson = given().config(
+                RestAssuredConfig.config()
+                    .encoderConfig(
+                        EncoderConfig.encoderConfig().
+                            encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                ).spec(specification)
+            .contentType(MediaType.APPLICATION_YAML_VALUE)
+            .accept(MediaType.APPLICATION_YAML_VALUE)
+                .body(person, objectMapper)
+            .when()
+                .post()
+            .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+            .extract()
+                .body()
+                    .as(PersonDTO.class, objectMapper);
+
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Linus", createdPerson.getFirstName());
+        assertEquals("Torvalds", createdPerson.getLastName());
+        assertEquals("Helsinki - Finland", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertTrue(createdPerson.getEnabled());
+
+    }
+    
+    @Test
+    @Order(2)
+    void updateTest() {
+        person.setLastName("Benedict Torvalds");
+
+        var createdPerson = given().config(
+                        RestAssuredConfig.config()
+                                .encoderConfig(
+                                        EncoderConfig.encoderConfig().
+                                                encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                ).spec(specification)
+            .contentType(MediaType.APPLICATION_YAML_VALUE)
+            .accept(MediaType.APPLICATION_YAML_VALUE)
+                .body(person, objectMapper)
+            .when()
+                .put()
+            .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+            .extract()
+                .body()
+                .as(PersonDTO.class, objectMapper);
+
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Linus", createdPerson.getFirstName());
+        assertEquals("Benedict Torvalds", createdPerson.getLastName());
+        assertEquals("Helsinki - Finland", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertTrue(createdPerson.getEnabled());
+
+    }
+
+    @Test
+    @Order(3)
+    void findByIdTest() {
+
+        var createdPerson = given().config(
+                        RestAssuredConfig.config()
+                                .encoderConfig(
+                                        EncoderConfig.encoderConfig().
+                                                encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                ).spec(specification)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .accept(MediaType.APPLICATION_YAML_VALUE)
+                    .pathParam("id", person.getId())
+                .when()
+                    .get("{id}")
+                .then()
+                    .statusCode(200)
+                    .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .extract()
+                    .body()
+                .as(PersonDTO.class, objectMapper);
+
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Linus", createdPerson.getFirstName());
+        assertEquals("Benedict Torvalds", createdPerson.getLastName());
+        assertEquals("Helsinki - Finland", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertTrue(createdPerson.getEnabled());
+    }
+
+    @Test
+    @Order(4)
+    void disableTest() {
+
+        var createdPerson = given().config(
+                        RestAssuredConfig.config()
+                                .encoderConfig(
+                                        EncoderConfig.encoderConfig().
+                                                encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT))
+                ).spec(specification)
+                .accept(MediaType.APPLICATION_YAML_VALUE)
+                    .pathParam("id", person.getId())
+                .when()
+                    .patch("{id}")
+                .then()
+                    .statusCode(200)
+                    .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .extract()
+                    .body()
+                .as(PersonDTO.class, objectMapper);
+
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Linus", createdPerson.getFirstName());
+        assertEquals("Benedict Torvalds", createdPerson.getLastName());
+        assertEquals("Helsinki - Finland", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertFalse(createdPerson.getEnabled());
+    }
+
+    @Test
+    @Order(5)
+    void deleteTest() {
+
+        given(specification)
+                .pathParam("id", person.getId())
+            .when()
+                .delete("{id}")
+            .then()
+                .statusCode(204);
+    }
+
+
+    @Test
+    @Order(6)
+    void findAllTest() {
+
+        var response = given(specification)
+                .accept(MediaType.APPLICATION_YAML_VALUE)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_YAML_VALUE)
+                .extract()
+                .body()
+                .as(PersonDTO[].class, objectMapper);
+
+        List<PersonDTO> people = Arrays.asList(response);
+
+        PersonDTO personOne = people.get(0);
+
+        assertNotNull(personOne.getId());
+        assertTrue(personOne.getId() > 0);
+
+        assertEquals("Leandro", personOne.getFirstName());
+        assertEquals("Pereira", personOne.getLastName());
+        assertEquals("Moradas do Bosque", personOne.getAddress());
+        assertEquals("Male", personOne.getGender());
+        assertTrue(personOne.getEnabled());
+    }
+
+    private void mockPerson() {
+        person.setFirstName("Linus");
+        person.setLastName("Torvalds");
+        person.setAddress("Helsinki - Finland");
+        person.setGender("Male");
+        person.setEnabled(true);
+    }
+}
