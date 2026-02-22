8108c5ff7c06eced93980c1a87f8bac4d681ed60
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-02-22 20:22:03 -0300

    sec-20-0.1-Initial Setup

---
+++ 17-query-params-pagination-hal/src/main/java/com/udemy/spring_boot/data/dto/PersonDTO.java
@@ -0,0 +1,84 @@
+package com.udemy.spring_boot.data.dto;
+
+import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTO extends RepresentationModel<PersonDTO> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+    private Boolean enabled;
+
+    public PersonDTO(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getEnabled(), personDTO.getEnabled());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getId(), getFirstName(), getLastName(), getAddress(), getGender(), getEnabled());
+    }
+}
---
+++ 17-query-params-pagination-hal/src/main/java/com/udemy/spring_boot/services/PersonServices.java
@@ -0,0 +1,111 @@
+package com.udemy.spring_boot.services;
+
+import com.udemy.spring_boot.controllers.PersonController;
+import com.udemy.spring_boot.data.dto.PersonDTO;
+import com.udemy.spring_boot.exception.RequiredObjectIsNullException;
+import com.udemy.spring_boot.exception.ResourceNotFoundException;
+import static com.udemy.spring_boot.mapper.ObjectMapper.parseObject;
+import static com.udemy.spring_boot.mapper.ObjectMapper.parseListObjects;
+import com.udemy.spring_boot.model.Person;
+import com.udemy.spring_boot.repository.PersonRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository personRepository;
+
+    public PersonDTO create(PersonDTO person) {
+
+        if (person == null) throw new RequiredObjectIsNullException();
+
+        logger.info("creating one person");
+
+        var entity = parseObject(person, Person.class);
+        var dto = parseObject(personRepository.save(entity), PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public List<PersonDTO> findAll() {
+        logger.info("finding all persons");
+        var people = parseListObjects(personRepository.findAll(), PersonDTO.class);
+        people.forEach(this::addHateoasLinks);
+        return people;
+    }
+
+    public PersonDTO findById(Long id) {
+        logger.info("finding one person");
+
+        var entity = personRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+        var dto = parseObject(entity, PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public PersonDTO update(PersonDTO person) {
+
+        if (person == null) throw new RequiredObjectIsNullException();
+
+        logger.info("updating one person");
+        Person entity = personRepository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        var dto = parseObject(personRepository.save(entity), PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    @Transactional
+    public PersonDTO disablePerson(Long id) {
+        logger.info("disabling one person");
+
+        personRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+
+        personRepository.disablePerson(id);
+
+        var entity = personRepository.findById(id).get();
+        var dto = parseObject(entity, PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public void delete(Long id) {
+        logger.info("deleting one person");
+
+        Person entity = personRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+
+        personRepository.delete(entity);
+    }
+
+    private void addHateoasLinks(PersonDTO dto) {
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).findAll()).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).create(dto)).withRel("create").withType("POST"));
+        dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
+        dto.add(linkTo(methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
+        dto.add(linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+    }
+}
