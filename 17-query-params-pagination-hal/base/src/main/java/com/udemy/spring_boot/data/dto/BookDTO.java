f83680e74580e24a192eb553f7fec3368be9f325
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-02-27 09:22:50 -0300

    sec-20: update base with final HAL implementation from challenge-1

--- 17-query-params-pagination-hal/base/src/main/java/com/udemy/spring_boot/data/dto/BookDTO.java
+++ 17-query-params-pagination-hal/base/src/main/java/com/udemy/spring_boot/data/dto/BookDTO.java
@@ -1,11 +1,13 @@
 package com.udemy.spring_boot.data.dto;
 
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Relation(collectionRelation = "books")
 public class BookDTO extends RepresentationModel<BookDTO> implements Serializable {
 
     private static final long serialVersionUID = 1L;
