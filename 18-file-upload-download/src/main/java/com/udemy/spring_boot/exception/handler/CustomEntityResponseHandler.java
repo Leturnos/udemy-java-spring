bd0550eed2464e11b6b248ec53f0dfcd9a78d36d
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-03-05 10:43:51 -0300

    sec-21: add file upload exceptions and UploadFileResponseDTO

--- 18-file-upload-download/src/main/java/com/udemy/spring_boot/exception/handler/CustomEntityResponseHandler.java
+++ 18-file-upload-download/src/main/java/com/udemy/spring_boot/exception/handler/CustomEntityResponseHandler.java
@@ -1,8 +1,6 @@
 package com.udemy.spring_boot.exception.handler;
 
-import com.udemy.spring_boot.exception.ExceptionResponse;
-import com.udemy.spring_boot.exception.RequiredObjectIsNullException;
-import com.udemy.spring_boot.exception.ResourceNotFoundException;
+import com.udemy.spring_boot.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,7 +16,7 @@ import java.util.Date;
 public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ExceptionResponse> handlerAllException(Exception ex, WebRequest request){
+    public final ResponseEntity<ExceptionResponse> handleAllException(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
@@ -27,7 +25,7 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public final ResponseEntity<ExceptionResponse> handlerNotFoundException(Exception ex, WebRequest request){
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
@@ -36,11 +34,29 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
     }
 
     @ExceptionHandler(RequiredObjectIsNullException.class)
-    public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request){
+    public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileNotFoundException(Exception ex, WebRequest request){
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileStorageException(Exception ex, WebRequest request){
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
