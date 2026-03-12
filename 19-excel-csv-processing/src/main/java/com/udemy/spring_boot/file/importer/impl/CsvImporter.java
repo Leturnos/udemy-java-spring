b4d43378f4dcbccbcdf5d4284d2ba04e6e2abafe
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-03-12 16:21:08 -0300

    sec-22: implement CSV and XLSX parsing logic with Apache Commons CSV and POI

--- 19-excel-csv-processing/src/main/java/com/udemy/spring_boot/file/importer/impl/CsvImporter.java
+++ 19-excel-csv-processing/src/main/java/com/udemy/spring_boot/file/importer/impl/CsvImporter.java
@@ -2,9 +2,13 @@ package com.udemy.spring_boot.file.importer.impl;
 
 import com.udemy.spring_boot.data.dto.PersonDTO;
 import com.udemy.spring_boot.file.importer.contract.FileImporter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -12,6 +16,29 @@ public class CsvImporter implements FileImporter {
 
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        return List.of();
+        CSVFormat format = CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true) // cada linha é um record
+                .setIgnoreEmptyLines(true)
+                .setTrim(true)
+                .get();
+
+        Iterable<CSVRecord> records = format.parse(new InputStreamReader(inputStream));
+        return parseRecordsToPersonDTOList(records);
+    }
+
+    private List<PersonDTO> parseRecordsToPersonDTOList(Iterable<CSVRecord> records) {
+        List<PersonDTO> people = new ArrayList<>();
+
+        for (CSVRecord record: records) {
+            PersonDTO person = new PersonDTO();
+            person.setFirstName(record.get("first_name"));
+            person.setLastName(record.get("last_name"));
+            person.setAddress(record.get("address"));
+            person.setGender(record.get("gender"));
+            person.setEnabled(true);
+            people.add(person);
+        }
+        return people;
     }
 }
--- 19-excel-csv-processing/src/main/java/com/udemy/spring_boot/file/importer/impl/XlsxImporter.java
+++ 19-excel-csv-processing/src/main/java/com/udemy/spring_boot/file/importer/impl/XlsxImporter.java
@@ -2,9 +2,16 @@ package com.udemy.spring_boot.file.importer.impl;
 
 import com.udemy.spring_boot.data.dto.PersonDTO;
 import com.udemy.spring_boot.file.importer.contract.FileImporter;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Component
@@ -12,6 +19,45 @@ public class XlsxImporter implements FileImporter {
 
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        return List.of();
+
+        try (XSSFWorkbook workbook = new XSSFWorkbook(inputStream)) {
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            if (rowIterator.hasNext()) rowIterator.next(); // se houver header
+
+            return parseRowsToPersonDTOList(rowIterator);
+        }
+    }
+
+    private List<PersonDTO> parseRowsToPersonDTOList(Iterator<Row> rowIterator) {
+        List<PersonDTO> people = new ArrayList<>();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            if (isRowValid(row)) {
+                people.add(parseRowToPersonDTO(row));
+            }
+        }
+
+        return people;
+    }
+
+    private PersonDTO parseRowToPersonDTO(Row row) {
+        DataFormatter formatter = new DataFormatter();
+        PersonDTO person = new PersonDTO();
+
+        // formatter.formatCellValue(cell) faz retornar "" se a célula for nula
+        person.setFirstName(formatter.formatCellValue(row.getCell(0)));
+        person.setLastName(formatter.formatCellValue(row.getCell(1)));
+        person.setAddress(formatter.formatCellValue(row.getCell(2)));
+        person.setGender(formatter.formatCellValue(row.getCell(3)));
+        person.setEnabled(true);
+
+        return person;
+    }
+
+    private static boolean isRowValid(Row row) {
+        return row.getCell(0) != null && row.getCell(0).getCellType() != CellType.BLANK;
     }
 }
